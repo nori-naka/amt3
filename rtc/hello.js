9e09791e1bb1fa56560269d6ea5d50221d0fd315
Noriaki <noriaki.nakamura@gmail.com>
2019-05-10 22:58:15 +0800

    0.3.0.2 debug

--- rtc/hello.js
+++ rtc/hello.js
@@ -1,14 +1,17 @@
 socketio.on("hello", function(msg){
+	alert("in hello")
 	const data = JSON.parse(msg);
 	
 	socketio.emit("hello-hello", {
 		dest: data.src,
 		src: local_id});
 	console.log("hello resive");
+	alert("out hello")
 });
 
 
 socketio.on("renew", function(msg){
+	alert("in renew");
 	const data = JSON.parse(msg);
 	
 	Object.keys(data).forEach(function(id){
@@ -18,13 +21,15 @@ socketio.on("renew", function(msg){
 				src :local_id
 			});
 	});
+	alert("out renew");
 })
 
 
 socketio.on("hello-hello", function(msg){
+	alert("in hello hello")
 	const data = JSON.parse(msg);
 	remotes[data.src].obj.$name.style.color = "green";
 	
 	start_audio_to(remotes[data.src]);
-	
+	alert("out hello hello")
 });
